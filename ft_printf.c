a7fab0e5f483fe8b6701de3d6b2544509e1b2636
Michael Weir <mweir@e1z3r5p2.42.us.org>
2018-06-08 17:47:04 -0700

    Changes to main

--- ft_printf.c
+++ ft_printf.c
@@ -40,3 +40,37 @@ int		valid_arg(int c)
 		|| c == '+' || c == 'l' || c == 'z'
 		|| c == 'j' || (c >= '0' && c <= '9')));
 }
+
+void	ft_parse(int flag, va_list arg, t_params *params)
+{
+	if (flag == 's')
+		print_str(arg);
+	else if (flag == 'd')
+		print_sint(arg, params);
+	else if (flag == 'u')
+		print_uint(arg, params);
+	else if (flag == 'c')
+		print_char(arg);
+	else if (flag == '%')
+		ft_putchar('%');
+	else if (flag == 'x' || flag == 'X')
+		print_hex(va_arg(arg, unsigned int), flag);
+	else if (flag == 'u')
+		print_uint(arg, params);
+	else if (flag == 'i')
+		print_sint(arg, params);
+}
+
+void	ft_printf(const char *frmt, ...)
+{
+	va_list		args;
+	t_params	*params;
+
+	params = ft_memalloc(sizeof(t_params));
+	if (frmt)
+	{
+		va_start(args, frmt);
+		parse_specifier(frmt, args, params);
+		va_end(args);
+	}
+}
