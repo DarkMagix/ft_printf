94bf9b9a64d791a04584717a7e202f8a1cba457c
DarkMagix <Mbeep16@hotmail.com>
2018-06-10 01:02:57 -0700

    Test

--- ft_printf.c
+++ ft_printf.c
@@ -47,7 +47,11 @@ void	ft_parse(int flag, va_list arg, t_params *params)
 	if (flag == 's')
 		print_str(arg);
 	else if (flag == 'd' || flag == 'D')
+	  {
+	    if (flag == 'D')
+	      params->modifer = MODI_L;
 		print_sint(arg, params);
+	  }
 	else if (flag == 'u')
 		print_uint(arg, params);
 	else if (flag == 'c')
